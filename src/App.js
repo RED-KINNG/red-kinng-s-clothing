9449170a6ecf0a15982b1f9913288ae5aafc7276
Red Kinng <68705440+RED-KINNG@users.noreply.github.com>
2021-01-10 00:49:28 -0800

    added product items and header component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1> HatsPage</h1>
-  </div>
-)
 
 function App() {
   return (
     <div >
-      <switch>
+      <Header/>
+      <Switch>
         <Route exact path='/' component = {HomePage} />
-        <Route path='/hats' component = {HatsPage} />
-      </switch>
+        <Route path='/shop' component = {ShopPage} />
+      </Switch>
         
     </div>
   );
